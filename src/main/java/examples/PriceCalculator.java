584d073805e65b086ea9f74778cf8f08cebfa1c4
bvegaM <vegabryam40@gmail.com>
2022-05-18 18:45:12 -0500

    passed all test

--- src/main/java/examples/PriceCalculator.java
+++ src/main/java/examples/PriceCalculator.java
@@ -1,8 +1,30 @@
 package examples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PriceCalculator {
 
+    private final List<Double> prices = new ArrayList<>();
+    private Integer discount;
+
     public Double getTotal(){
-        return 0.0;
+        if (discount == null){
+            throw new IllegalArgumentException("Discount is not set");
+        }
+        Double total = this.prices.stream().mapToDouble(Double::doubleValue).sum();
+        Double discount = total * this.discount / 100;
+        return total - discount;
+    }
+
+    public void add(Double price){
+        prices.add(price);
+    }
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
     }
 }
--- src/test/java/examples/PriceCalculatorShould.java
+++ src/test/java/examples/PriceCalculatorShould.java
@@ -7,12 +7,43 @@ public class PriceCalculatorShould {
 
     /*
     * create a test that returns a 0.00 price of total
+    * create a test that returns total price of list total
+    * create a test that returns total price of list total with a discount
+    * create a exception when discount parameter not exist
      */
 
     @Test
     public void return_0_00_when_no_items_are_added() {
         PriceCalculator priceCalculator = new PriceCalculator();
+        priceCalculator.setDiscount(0);
         assertEquals(0.00, priceCalculator.getTotal(), 0.01);
+    }
+
+    @Test
+    public void return_total_price_of_list_total() {
+        PriceCalculator priceCalculator = new PriceCalculator();
+        priceCalculator.add(10.00);
+        priceCalculator.add(20.00);
+        priceCalculator.setDiscount(0);
+        assertEquals(30.00, priceCalculator.getTotal(), 0.01);
+    }
+
+    @Test
+    public void return_total_price_of_list_total_with_discount(){
+        PriceCalculator priceCalculator = new PriceCalculator();
+        priceCalculator.add(10.00);
+        priceCalculator.add(20.00);
+        priceCalculator.add(30.00);
+        priceCalculator.setDiscount(12);
+        assertEquals(52.80, priceCalculator.getTotal(), 0.10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void return_exception_when_discount_dont_exist(){
+        PriceCalculator priceCalculator = new PriceCalculator();
+        priceCalculator.add(10.00);
+        priceCalculator.add(20.00);
+        assertEquals(30.00, priceCalculator.getTotal(), 0.01);
     }
 
 }
